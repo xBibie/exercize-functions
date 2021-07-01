0dc60b8b471a4a4156043b72860835e5a9b7d7ba
Bibi <84073518+xBibie@users.noreply.github.com>
2021-07-01 22:28:57 +0200

    Add files via upload

---
+++ number.js
@@ -0,0 +1,42 @@
+
+const number = 110
+
+function bigNumber(number) {
+    if (number >= 100) {
+      return true;
+    } else {
+      return false;
+    }
+    console.log (number);
+  };
+  
+//a function that produce something
+
+//Brenda the Bouncer Bot
+
+const age = 21;
+const totalAmount = 126;
+const maxAmount = 125;
+
+if (totalAmount >125){
+    console.log ('its too busy now, come back later');
+} else {
+    console.log ('come in');
+};
+if (age < 18); {
+    console.log('This is a club for adults');
+};
+
+// a functions that produce something
+
+const calculateAverage = function (numbers){
+    calculateAverage (grades);
+    Math.round (grades);
+    return calculateAverage;
+}; 
+    const average = numbers.reduce ((total n)=> total+n)/numbers.length;
+    const grades = [5, 6, 8, 14, 78];
+    console.log (grades)
+
+    // a function that produce and does something
+    
